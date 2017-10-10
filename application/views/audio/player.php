52204dc5e1176b0864655f4953d9cbe138981935
suresh07 <soori_rozee@yahoo.com>
2017-10-10 21:23:04 +0530

    Core: aufio player fixes

--- application/views/audio/player.php
+++ application/views/audio/player.php
@@ -1,40 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Basic Page Needs
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <meta charset="utf-8">
 
-    <!-- Mobile Specific Metas
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Javascript calls
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-	<!-- <script src="//cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/1.4.0/wavesurfer.min.js"></script> -->
 	<link rel="stylesheet" href="https://cdn.plyr.io/2.0.16/plyr.css">
-
-    <!-- CSS
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/normalize.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <!-- <link rel="stylesheet" href="css/skeleton.css"> -->
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/viewer.css">
 </head>
 <body>
 
-<img src="<?=$data['cover']?>" class="img-responsive">
-<audio controls>
-  <source src="<?=DATA_URL . $data['id']?>/index.mp3" type="audio/mp3">
-  <source src="<?=DATA_URL . $data['id']?>/index.ogg" type="audio/ogg">
-</audio>
-
-<script src="https://cdn.plyr.io/2.0.16/plyr.js"></script>
-<script>plyr.setup();</script>
+    <div class="text-center">
+        <img style="margin: 0 auto;" src="<?=$data['cover']?>" class="img-responsive">
+        <audio controls>
+          <source src="<?=DATA_URL . $data['id']?>/index.mp3" type="audio/mp3">
+          <source src="<?=DATA_URL . $data['id']?>/index.ogg" type="audio/ogg">
+        </audio>
+    </div>
+    <script src="https://cdn.plyr.io/2.0.16/plyr.js"></script>
+    <script>plyr.setup();</script>
 
 </body>
 </html>
