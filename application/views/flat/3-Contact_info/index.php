e89ee96f432873ea4d67dbd8eb312e848af8e8b1
shivasdst <shivably04sdst@gmail.com>
2018-05-25 14:43:46 +0530

    Added contact info

---
+++ application/views/flat/3-Contact_info/index.php
@@ -0,0 +1,50 @@
+<div class="container">
+    <div class="row">
+        <!-- Column 1 -->
+        <div class="col-md-12 text-center">
+            <p>&nbsp;</p>    
+        </div>
+    </div>
+</div>
+<div class="container gap-above">
+    <div class="row">
+        <div class="col-md-9 main">
+            <h1>Contact Info</h1>
+		    <div class="row">
+				<div class="col-md-3">	
+					<h2>
+						<span class="contact-info"><i class="fa fa-map-marker"></i>&nbsp;Address</span><br />
+						Heritage Centre<br />
+						Ground Floor,<br />
+						Administration Building<br />
+						IIT Madras,<br />
+						Chennai 600036<br /><br />
+					</h2>
+				</div>	
+				<div class="col-md-3">	
+					<h2>
+						<span class="contact-info"><i class="fa fa-envelope"></i>&nbsp;E-Mail ID</span><br />
+						heritage@iitm.ac.in
+					</h2>
+				</div>	
+				<div class="col-md-3">	
+					<h2>
+						<span class="contact-info"><i class="fa fa-phone-square"></i>&nbsp;Phone Number</span><br />
+						+91-44-22579480
+					</h2>
+				</div>	
+				<div class="col-md-3">	
+					<h2>
+						<span class="contact-info"><i class="fa fa-link"></i>&nbsp;Website</span><br />
+						heritage.iitm.ac.in
+					</h2>
+				</div>	
+			</div>	
+
+            <div class="responive-map">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d577.910567863082!2d80.23277238140503!3d12.990812801406602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52678017713383%3A0xe8c8fcb681bdfb99!2sHeritage+Centre%2C+IIT+Madras!5e0!3m2!1sen!2sin!4v1527236824150" width="850" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
