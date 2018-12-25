fe86a0a44d3bbcffea17ca9784302defe61d5bae
shivasdst <shivably04sdst@gmail.com>
2018-12-25 17:48:46 +0530

    Project: moved about to separate page

---
+++ application/views/flat/4-About/index.php
@@ -0,0 +1,65 @@
+<div class="container main gap-above-large" id="about">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>About</h1>
+            <p>The Heritage Centre was formally inaugurated by Dr. Arcot Ramachandran, former Director IIT Madras on 3rd March 2006. The Centre is located on the ground floor of the administration building. The actual idea of a Heritage Centre was mooted in the year 2000 and it has become a reality due to the efforts of the Professor-in-charge Dr. Ajit Kumar Kolar and his team. The Centre will function as a repository of material of heritage value and historical significance of various facets of the Institute.</p>
+            <p>The exhibits include photographs, documents, publications, paintings, portraits, products developed and other articles. Information regarding important events, laboratory development, visits of important dignitaries, Indo-German cooperative activities, and academic achievements of faculty and students also are included. Aspects of IITM campus features and development, campus life and student activities are also included, thus broadening the scope of the Centre in the future to non-academic activities also.</p>
+            <p>The activities of the Heritage Centre will be of a continuous nature from now on and hence the support and cooperation of all IITians (students, faculty and staff, past and present) is very essential in making the Centre meet its goal of preserving IITM history and culture for the future generations of IITians. By the very nature of the task, the role of the Alumni is crucial in establishing and furtherance the Centre.</p>
+        </div>
+    </div>
+</div>      
+
+
+
+<div class="container" id="about">
+    <ul class="timeline">
+        <li>
+          <div class="timeline-badge">1956</div>
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>Visit of Prime Minister Jawaharlal Nehru to West Germany and the offer made to him by the Government of the Federal Republic of Germany to assist in the setting up of a higher technological institute in India.</p>
+            </div>
+          </div>
+        </li>
+        <li class="timeline-inverted">
+          <div class="timeline-badge">1958</div>
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>The signing of the first Indo-German agreement in Bonn, West Germany for the establishment of the Indian Institute of Technology at Madras.</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="timeline-badge">1959</div>
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>Formal inauguration of the Institute by Prof.Humayun Kabir, Union Minister for Scientific Research and Cultural Affairs, Dr.A.Lakshmanaswamy Mudaliar assumes office as the first Chairman of the Board of Governors of the Institute. Prof.B.Sengupto assumes office as the first Director of the Institute.</p>
+            </div>
+          </div>
+        </li>
+        <li class="timeline-inverted">
+          <div class="timeline-badge">1961</div>
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>Declaration of the Institute by the Parliament of India as an institution of National importance.</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="timeline-badge">1962</div>
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>Visit to the Institute by Dr.Heinrich Lubke, President of the Federal Republic of Germany and the unveiling of a tablet symbolising the Indo-German technical assistance programme.</p>
+            </div>
+          </div>
+        </li>
+        <li class="timeline-inverted">
+          <div class="timeline-badge">1967</div>
+          <div class="timeline-panel">
+            <div class="timeline-body">
+              <p>Dr. A. Ramachandran takes over as Director from Prof. B. Sengupto.</p>
+            </div>
+          </div>
+        </li>
+    </ul>
+</div>
