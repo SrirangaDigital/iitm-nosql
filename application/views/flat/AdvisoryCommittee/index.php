540866b14867c34cf45b66ecde3ee5352afb516a
Shivashankar <shivably04sdst@gmail.com>
2019-11-25 17:41:11 +0530

    Added advisory committee page

---
+++ application/views/flat/AdvisoryCommittee/index.php
@@ -0,0 +1,85 @@
+<div class="container main gap-above-large" id="about">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Advisory Committee</h1>
+        </div>
+    </div>
+
+<div class="row">
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="<?=PUBLIC_URL?>images/stock/adcommittee/msa.jpg" alt="Prof. M.S. Ananth">
+      <div class="caption">
+        <h4>Prof. M.S. Ananth</h4>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="<?=PUBLIC_URL?>images/stock/adcommittee/Prof_Nagarajan.jpg" alt="Prof. R. Nagarajan">
+      <div class="caption">
+        <h4>Prof. R. Nagarajan</h4>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="<?=PUBLIC_URL?>images/stock/adcommittee/Mahesh_Panchagnula.jpg" alt="Prof. Mahesh Panchagnula">
+      <div class="caption">
+        <h4>Prof. Mahesh Panchagnula</h4>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="<?=PUBLIC_URL?>images/stock/adcommittee/avatar.png" alt="Prof. Koshy Varghese">
+      <div class="caption">
+        <h4>Prof. Koshy Varghese</h4>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="<?=PUBLIC_URL?>images/stock/adcommittee/avatar.png" alt="Prof. P. Sriram">
+      <div class="caption">
+        <h4>Prof. P. Sriram</h4>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="<?=PUBLIC_URL?>images/stock/adcommittee/avatar.png" alt="Prof. C.S. Swamy">
+      <div class="caption">
+        <h4>Prof. C.S. Swamy</h4>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="<?=PUBLIC_URL?>images/stock/adcommittee/santhakumar.jpg" alt="Prof. Santhakumar">
+      <div class="caption">
+        <h4>Prof. Santhakumar</h4>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="<?=PUBLIC_URL?>images/stock/adcommittee/avatar.png" alt="Mr. S. Srinivasan">
+      <div class="caption">
+        <h4>Mr. S. Srinivasan</h4>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="<?=PUBLIC_URL?>images/stock/adcommittee/avatar.png" alt="Mr. Kumaran Sathasivam">
+      <div class="caption">
+        <h4>Mr. Kumaran Sathasivam</h4>
+      </div>
+    </div>
+  </div>
+</div>
+</div>      
+
+
+
