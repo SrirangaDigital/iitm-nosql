53d8b605ef6d8122ea44c943e805379c7fd0ad3c
Shivashankar <shivably04sdst@gmail.com>
2020-12-29 12:13:08 +0530

    updated page details

--- application/views/flat/TreasureHunt/index.php
+++ application/views/flat/TreasureHunt/index.php
@@ -10,28 +10,18 @@
     <p><img src="<?=PUBLIC_URL?>images/stock/forthcoming-event/Heritage_Trail.jpg" alt=""/></p>
   </div>  
   <div class="col-md-8">
-    <p>Enjoy 40 minutes of treasure hunt fun exploration from your mobiles with family and friends. You will need to crack 18-odd questions (puzzles/riddles/video clues and others). Of course, some of you may need more than 40 mins while others will ace it in much lesser time. And exciting prizes for the top scorers.</p>   
-    <p>Please mark your calendars right away</p>
-    <p>
-      India, EMEA, Africa, Americas and other geos (West of India)<br /> 
-      &nbsp;&nbsp;&nbsp; &mdash; Date:   <strong>29th December</strong><br />  
-      &nbsp;&nbsp;&nbsp; &mdash; Time:   <strong>8 PM IST to 11 PM IST</strong><br />
-      India, Singapore, Australia and other geos (East of India)<br /> 
-      &nbsp;&nbsp;&nbsp; &mdash; Date:   <strong>30th December</strong><br />  
-      &nbsp;&nbsp;&nbsp; &mdash; Time:   <strong>1 PM IST to  4 PM IST</strong>
-    </p>
-    <p>  
-      2-minute Installation and Prep
-    </p>
+    <p>Enjoy 40 minutes of treasure hunt fun exploration from your mobiles with family and friends. You will need to crack about 20-odd questions (puzzles/riddles/video clues and others). Of course, some of you may need more and others will ace faster. And exciting prizes for the top scorers.</p>   
+    <p>Event starts at <strong>29th Dec 2020 at 8:00 PM IST</strong> and will be open until <strong>4th Jan 2020 8:00 AM IST</strong> to cover all geos.</p>
+    <p>2-minute Installation and Prep</p>
     <ol>
       <li><a href="https://urlprotection-tko.global.sonicwall.com/click?PV=1&MSGID=202012230504270173252&URLID=1&ESV=10.0.6.3447&IV=A218CCFDEC33DD4DD0826D5917CD1456&TT=1608699870823&ESN=6UDumVWDJWJVdallOtH8cxqE6Ql9Y%2Fg8gY67N7JJPT8%3D&KV=1536961729279&ENCODED_URL=http%3A%2F%2Fonelink.to%2Fgamifyi&HK=B8937F0E30A431E9A4F4BB6C4BE6011E74F35BEAAD86EE7C5AB82EDB59CB1E96">Download Gamifyi Mobile App</a></li>
       <li>Click on Quests > Select City - IITM (top left)</li>
       <li>Open - Peak IITM Nostalgia 2020 > Proceed</li>
       <li>Register in the App- we need this to maintain points and update results to you</li>
+      <li>The game will be made LIVE during the time slots indicated...</li>
     </ol>
-    <p>The game will be made LIVE during the time slots indicated...</p>
-    <p><strong>IMPORTANT POINTS TO NOTE:</strong></p>
-    <ul class="bulletedstyle">
+    <h3>IMPORTANT POINTS TO NOTE:</h3>
+    <ul class="bulletedstyle">  
       <li>Play the game on a mobile device - Android 7 &amp; above, iOS 9 and above (Does not support Tablets, Desktops)</li> 
       <li>App requests permissions for storage, camera, and location. Enable these permissions for a seamless experience.</li>
       <li>For Photo Activities you will be immediately awarded points on submitting the photo. All photos will be validated after game completion. Incorrect photos will lead to the reduction of your final scores.</li>
